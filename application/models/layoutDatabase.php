6c11d2925c439dd044c90e8ff9f9c1aac6374724
ScottRamsden <scott.ramsden@byng-systems.com>
2012-11-27 12:00:33 +0000

    Adding layout

---
+++ application/models/layoutDatabase.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by scott
+ * Date: 27/11/12
+ * Time: 11:53
+ */
+
+/**
+ * This file connects to mysql and pulls data used on the layout page.
+ */
+
+$mysqlconnect;
+
+
+
+// $this->sitename = $mysqlresultsitename;
+
